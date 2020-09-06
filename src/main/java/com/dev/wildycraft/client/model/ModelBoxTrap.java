addccc7087980d5546574b6021e9e65b6de93273
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-09-06 19:07:31 -0400

    updated BoxTrap model
    updated boxtrap model to 1.12.2

--- src/main/java/com/dev/wildycraft/client/model/ModelBoxTrap.java
+++ src/main/java/com/dev/wildycraft/client/model/ModelBoxTrap.java
@@ -1,292 +1,173 @@
-package wildycraft.client.model;
+// Made with Blockbench 3.6.6
+// Exported for Minecraft version 1.12
+// Paste this class into your mod and generate all required imports
+
+package com.dev.wildycraft.client.model;
 
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelBoxTrap extends ModelBase
-{
-    ModelRenderer Center;
-    ModelRenderer Side1;
-    ModelRenderer Side2;
-    ModelRenderer Side3;
-    ModelRenderer Side4;
-    ModelRenderer Pillar1;
-    ModelRenderer Pillar2;
-    ModelRenderer Pillar3;
-    ModelRenderer Pillar4;
-    ModelRenderer Stick;
-    ModelRenderer Rock;
-    ModelRenderer Support1;
-    ModelRenderer Support2;
-    ModelRenderer Base;
-    ModelRenderer String1;
-    ModelRenderer String2;
-    ModelRenderer String3;
-    ModelRenderer String4;
-    ModelRenderer String5;
-    ModelRenderer String6;
-    ModelRenderer String7;
-    ModelRenderer String8;
-  
-  public ModelBoxTrap()
-  {
-    textureWidth = 64;
-    textureHeight = 32;
-    
-      Center = new ModelRenderer(this, 0, 0);
-      Center.addBox(-7F, 0F, -7F, 14, 1, 14);
-      Center.setRotationPoint(0F, 10F, 0F);
-      Center.setTextureSize(64, 32);
-      Center.mirror = true;
-      setRotation(Center, 0F, 0F, 0F);
-      Side1 = new ModelRenderer(this, 0, 0);
-      Side1.addBox(-7F, 0F, 0F, 14, 1, 14);
-      Side1.setRotationPoint(0F, 10F, 7F);
-      Side1.setTextureSize(64, 32);
-      Side1.mirror = true;
-      setRotation(Side1, 0F, 0F, 0F);
-      Side2 = new ModelRenderer(this, 0, 0);
-      Side2.addBox(-7F, 0F, -14F, 14, 1, 14);
-      Side2.setRotationPoint(-7F, 10F, 0F);
-      Side2.setTextureSize(64, 32);
-      Side2.mirror = true;
-      setRotation(Side2, 0F, 1.570796F, 0F);
-      Side3 = new ModelRenderer(this, 0, 0);
-      Side3.addBox(-7F, 0F, 0F, 14, 1, 14);
-      Side3.setRotationPoint(7F, 10F, 0F);
-      Side3.setTextureSize(64, 32);
-      Side3.mirror = true;
-      setRotation(Side3, 0F, 1.570796F, 0F);
-      Side4 = new ModelRenderer(this, 0, 0);
-      Side4.addBox(-7F, 0F, -14F, 14, 1, 14);
-      Side4.setRotationPoint(0F, 10F, -7F);
-      Side4.setTextureSize(64, 32);
-      Side4.mirror = true;
-      setRotation(Side4, 0F, 0F, 0F);
-      Pillar1 = new ModelRenderer(this, 56, 0);
-      Pillar1.addBox(0F, 0.1F, 0F, 1, 14, 1);
-      Pillar1.setRotationPoint(-6.9F, 10F, -6.9F);
-      Pillar1.setTextureSize(64, 32);
-      Pillar1.mirror = true;
-      setRotation(Pillar1, 0F, 0F, 0F);
-      Pillar2 = new ModelRenderer(this, 56, 0);
-      Pillar2.addBox(0F, 0.1F, 0F, 1, 14, 1);
-      Pillar2.setRotationPoint(5.9F, 10F, -6.9F);
-      Pillar2.setTextureSize(64, 32);
-      Pillar2.mirror = true;
-      setRotation(Pillar2, 0F, 0F, 0F);
-      Pillar3 = new ModelRenderer(this, 56, 0);
-      Pillar3.addBox(0F, 0F, 0F, 1, 14, 1);
-      Pillar3.setRotationPoint(-7F, 10F, 6.133333F);
-      Pillar3.setTextureSize(64, 32);
-      Pillar3.mirror = true;
-      setRotation(Pillar3, 0F, 0F, 0F);
-      Pillar4 = new ModelRenderer(this, 56, 0);
-      Pillar4.addBox(0F, 0.1F, 0F, 1, 14, 1);
-      Pillar4.setRotationPoint(5.9F, 10F, 5.9F);
-      Pillar4.setTextureSize(64, 32);
-      Pillar4.mirror = true;
-      setRotation(Pillar4, 0F, 0F, 0F);
-      Stick = new ModelRenderer(this, 60, 0);
-      Stick.addBox(0F, 0F, 0F, 1, 14, 1);
-      Stick.setRotationPoint(-3F, -4F, 3F);
-      Stick.setTextureSize(64, 32);
-      Stick.mirror = true;
-      setRotation(Stick, 0F, 0F, 0F);
-      Rock = new ModelRenderer(this, 0, 17);
-      Rock.addBox(0F, -1.5F, -1.5F, 3, 3, 3);
-      Rock.setRotationPoint(-2F, -3F, 3F);
-      Rock.setTextureSize(64, 32);
-      Rock.mirror = true;
-      setRotation(Rock, 0F, 0.9666439F, 0F);
-      Support1 = new ModelRenderer(this, 0, 15);
-      Support1.addBox(0.2F, -0.5F, -0.5F, 7, 1, 1);
-      Support1.setRotationPoint(-3F, 8F, 3.3F);
-      Support1.setTextureSize(64, 32);
-      Support1.mirror = true;
-      setRotation(Support1, 0F, -0.2230717F, 0.3543328F);
-      Support2 = new ModelRenderer(this, 0, 15);
-      Support2.addBox(0F, -0.5F, -0.5F, 7, 1, 1);
-      Support2.setRotationPoint(-2.5F, 8F, 3.3F);
-      Support2.setTextureSize(64, 32);
-      Support2.mirror = true;
-      setRotation(Support2, 0F, 1.896109F, 0.3717861F);
-      Base = new ModelRenderer(this, 2, 15);
-      Base.addBox(-7F, 0F, -7F, 14, 0, 14);
-      Base.setRotationPoint(0F, 23.7F, 0F);
-      Base.setTextureSize(64, 32);
-      Base.mirror = true;
-      setRotation(Base, 0F, 0F, 0F);
-      String1 = new ModelRenderer(this, 0, 29);
-      String1.addBox(-1F, -0.5F, 0F, 19, 1, 0);
-      String1.setRotationPoint(7F, 23F, -6.5F);
-      String1.setTextureSize(64, 32);
-      String1.mirror = true;
-      setRotation(String1, 0F, 0F, -0.7807508F);
-      String2 = new ModelRenderer(this, 0, 29);
-      String2.addBox(-1F, -0.5F, 0F, 19, 1, 0);
-      String2.setRotationPoint(7F, 23F, 6.5F);
-      String2.setTextureSize(64, 32);
-      String2.mirror = true;
-      setRotation(String2, 0F, 0F, -0.7807508F);
-      String3 = new ModelRenderer(this, 0, 29);
-      String3.addBox(-1F, -0.5F, 0F, 19, 1, 0);
-      String3.setRotationPoint(6.5F, 23F, -7F);
-      String3.setTextureSize(64, 32);
-      String3.mirror = true;
-      setRotation(String3, 0F, 1.570796F, -0.7807508F);
-      String4 = new ModelRenderer(this, 0, 29);
-      String4.addBox(-1F, -0.5F, 0F, 19, 1, 0);
-      String4.setRotationPoint(-6.5F, 23F, -7F);
-      String4.setTextureSize(64, 32);
-      String4.mirror = true;
-      setRotation(String4, 0F, 1.570796F, -0.7807508F);
-      String5 = new ModelRenderer(this, 0, 29);
-      String5.addBox(-1F, -0.5F, 0F, 19, 1, 0);
-      String5.setRotationPoint(6.5F, 23F, 7F);
-      String5.setTextureSize(64, 32);
-      String5.mirror = true;
-      setRotation(String5, 0F, -1.570796F, -0.7807508F);
-      String6 = new ModelRenderer(this, 0, 29);
-      String6.addBox(-1F, -0.5F, 0F, 19, 1, 0);
-      String6.setRotationPoint(-6.5F, 23F, 7F);
-      String6.setTextureSize(64, 32);
-      String6.mirror = true;
-      setRotation(String6, 0F, -1.570796F, -0.7807508F);
-      String7 = new ModelRenderer(this, 0, 29);
-      String7.addBox(-1F, -0.5F, 0F, 19, 1, 0);
-      String7.setRotationPoint(-7F, 23F, 6.5F);
-      String7.setTextureSize(64, 32);
-      String7.mirror = true;
-      setRotation(String7, 0F, 3.141593F, -0.7807508F);
-      String8 = new ModelRenderer(this, 0, 29);
-      String8.addBox(-1F, -0.5F, 0F, 19, 1, 0);
-      String8.setRotationPoint(-7F, 23F, -6.5F);
-      String8.setTextureSize(64, 32);
-      String8.mirror = true;
-      setRotation(String8, 0F, 3.141593F, -0.7807508F);
-  }
-  
-  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-    Center.render(f5);
-    Side1.render(f5);
-    Side2.render(f5);
-    Side3.render(f5);
-    Side4.render(f5);
-    Pillar1.render(f5);
-    Pillar2.render(f5);
-    Pillar3.render(f5);
-    Pillar4.render(f5);
-    Stick.render(f5);
-    Rock.render(f5);
-    Support1.render(f5);
-    Support2.render(f5);
-    Base.render(f5);
-    String1.render(f5);
-    String2.render(f5);
-    String3.render(f5);
-    String4.render(f5);
-    String5.render(f5);
-    String6.render(f5);
-    String7.render(f5);
-    String8.render(f5);
-  }
-  
-  public void renderModel(float f5)
-  {
-    Center.render(f5);
-    Side1.render(f5);
-    Side2.render(f5);
-    Side3.render(f5);
-    Side4.render(f5);
-    Pillar1.render(f5);
-    Pillar2.render(f5);
-    Pillar3.render(f5);
-    Pillar4.render(f5);
-    Stick.render(f5);
-    Rock.render(f5);
-    Support1.renderWithRotation(f5);
-    Support2.renderWithRotation(f5);
-    Base.render(f5);
-    String1.renderWithRotation(f5);
-    String2.renderWithRotation(f5);
-    String3.renderWithRotation(f5);
-    String4.renderWithRotation(f5);
-    String5.renderWithRotation(f5);
-    String6.renderWithRotation(f5);
-    String7.renderWithRotation(f5);
-    String8.renderWithRotation(f5);
-  }
-  
-  public void renderModelClosed(float f5)
-  {
-    Center.render(f5);
-    Side1.rotateAngleX = -(float)Math.PI/2;
-    Side1.render(f5);
-    Side2.rotateAngleZ = - (float)Math.PI/2;
-    Side2.render(f5);
-    Side3.rotateAngleX = -(float)Math.PI/2;
-    Side3.render(f5);
-    Side4.rotateAngleX = (float)Math.PI/2;
-    Side4.render(f5);
-    Pillar1.render(f5);
-    Pillar2.render(f5);
-    Pillar3.render(f5);
-    Pillar4.render(f5);
-    Stick.render(f5);
-    Rock.render(f5);
-    Support1.renderWithRotation(f5);
-    Support2.renderWithRotation(f5);
-    Base.render(f5);
-  }
-  
-  public void renderModel(float f5, int time)
-  {
-    Center.render(f5);
-    Side1.rotateAngleX = -(float)((Math.PI/2)*((time)/4.0));
-    Side1.render(f5);
-    Side2.rotateAngleZ = - (float)((Math.PI/2)*((time)/4.0));
-    Side2.render(f5);
-    Side3.rotateAngleX = -(float)((Math.PI/2)*((time)/4.0));
-    Side3.render(f5);
-    Side4.rotateAngleX = (float)((Math.PI/2)*((time)/4.0));
-    Side4.render(f5);
-    Pillar1.render(f5);
-    Pillar2.render(f5);
-    Pillar3.render(f5);
-    Pillar4.render(f5);
-    Stick.render(f5);
-    Rock.setRotationPoint(-2F, -3F + time * 3, 3F);;
-    Rock.render(f5);
-    Support1.renderWithRotation(f5);
-    Support2.renderWithRotation(f5);
-    Base.render(f5);
-    if(time == 0){
-    	String1.renderWithRotation(f5);
-    	String2.renderWithRotation(f5);
-    	String3.renderWithRotation(f5);
-    	String4.renderWithRotation(f5);
-    	String5.renderWithRotation(f5);
-    	String6.renderWithRotation(f5);
-    	String7.renderWithRotation(f5);
-    	String8.renderWithRotation(f5);
-    }
-  }
-  
-  private void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
-  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
-  {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-  }
+public class ModelBoxTrap extends ModelBase {
+	private final ModelRenderer Center;
+	private final ModelRenderer Side1;
+	private final ModelRenderer Side2;
+	private final ModelRenderer Side3;
+	private final ModelRenderer Side4;
+	private final ModelRenderer Pillar1;
+	private final ModelRenderer Pillar2;
+	private final ModelRenderer Pillar3;
+	private final ModelRenderer Pillar4;
+	private final ModelRenderer Stick;
+	private final ModelRenderer Rock;
+	private final ModelRenderer Support1;
+	private final ModelRenderer Support2;
+	private final ModelRenderer Base;
+	private final ModelRenderer String1;
+	private final ModelRenderer String2;
+	private final ModelRenderer String3;
+	private final ModelRenderer String4;
+	private final ModelRenderer String5;
+	private final ModelRenderer String6;
+	private final ModelRenderer String7;
+	private final ModelRenderer String8;
 
+	public ModelBoxTrap() {
+		textureWidth = 64;
+		textureHeight = 32;
+
+		Center = new ModelRenderer(this);
+		Center.setRotationPoint(0.0F, 10.0F, 0.0F);
+		Center.cubeList.add(new ModelBox(Center, 0, 0, -7.0F, 0.0F, -7.0F, 14, 1, 14, 0.0F, true));
+
+		Side1 = new ModelRenderer(this);
+		Side1.setRotationPoint(0.0F, 10.0F, 7.0F);
+		Side1.cubeList.add(new ModelBox(Side1, 0, 0, -7.0F, 0.0F, 0.0F, 14, 1, 14, 0.0F, true));
+
+		Side2 = new ModelRenderer(this);
+		Side2.setRotationPoint(-7.0F, 10.0F, 0.0F);
+		setRotationAngle(Side2, 0.0F, 1.5708F, 0.0F);
+		Side2.cubeList.add(new ModelBox(Side2, 0, 0, -7.0F, 0.0F, -14.0F, 14, 1, 14, 0.0F, true));
+
+		Side3 = new ModelRenderer(this);
+		Side3.setRotationPoint(7.0F, 10.0F, 0.0F);
+		setRotationAngle(Side3, 0.0F, 1.5708F, 0.0F);
+		Side3.cubeList.add(new ModelBox(Side3, 0, 0, -7.0F, 0.0F, 0.0F, 14, 1, 14, 0.0F, true));
+
+		Side4 = new ModelRenderer(this);
+		Side4.setRotationPoint(0.0F, 10.0F, -7.0F);
+		Side4.cubeList.add(new ModelBox(Side4, 0, 0, -7.0F, 0.0F, -14.0F, 14, 1, 14, 0.0F, true));
+
+		Pillar1 = new ModelRenderer(this);
+		Pillar1.setRotationPoint(-6.9F, 10.0F, -6.9F);
+		Pillar1.cubeList.add(new ModelBox(Pillar1, 56, 0, 0.0F, 0.1F, 0.0F, 1, 14, 1, 0.0F, true));
+
+		Pillar2 = new ModelRenderer(this);
+		Pillar2.setRotationPoint(5.9F, 10.0F, -6.9F);
+		Pillar2.cubeList.add(new ModelBox(Pillar2, 56, 0, 0.0F, 0.1F, 0.0F, 1, 14, 1, 0.0F, true));
+
+		Pillar3 = new ModelRenderer(this);
+		Pillar3.setRotationPoint(-7.0F, 10.0F, 6.1333F);
+		Pillar3.cubeList.add(new ModelBox(Pillar3, 56, 0, 0.0F, 0.0F, 0.0F, 1, 14, 1, 0.0F, true));
+
+		Pillar4 = new ModelRenderer(this);
+		Pillar4.setRotationPoint(5.9F, 10.0F, 5.9F);
+		Pillar4.cubeList.add(new ModelBox(Pillar4, 56, 0, 0.0F, 0.1F, 0.0F, 1, 14, 1, 0.0F, true));
+
+		Stick = new ModelRenderer(this);
+		Stick.setRotationPoint(-3.0F, -4.0F, 3.0F);
+		Stick.cubeList.add(new ModelBox(Stick, 60, 0, 0.0F, 0.0F, 0.0F, 1, 14, 1, 0.0F, true));
+
+		Rock = new ModelRenderer(this);
+		Rock.setRotationPoint(-2.0F, -3.0F, 3.0F);
+		setRotationAngle(Rock, 0.0F, 0.9666F, 0.0F);
+		Rock.cubeList.add(new ModelBox(Rock, 0, 17, 0.0F, -1.5F, -1.5F, 3, 3, 3, 0.0F, true));
+
+		Support1 = new ModelRenderer(this);
+		Support1.setRotationPoint(-3.0F, 8.0F, 3.3F);
+		setRotationAngle(Support1, 0.0F, -0.2231F, 0.3543F);
+		Support1.cubeList.add(new ModelBox(Support1, 0, 15, 0.2F, -0.5F, -0.5F, 7, 1, 1, 0.0F, true));
+
+		Support2 = new ModelRenderer(this);
+		Support2.setRotationPoint(-2.5F, 8.0F, 3.3F);
+		setRotationAngle(Support2, 0.0F, 1.8961F, 0.3718F);
+		Support2.cubeList.add(new ModelBox(Support2, 0, 15, 0.0F, -0.5F, -0.5F, 7, 1, 1, 0.0F, true));
+
+		Base = new ModelRenderer(this);
+		Base.setRotationPoint(0.0F, 23.7F, 0.0F);
+		Base.cubeList.add(new ModelBox(Base, 2, 15, -7.0F, 0.0F, -7.0F, 14, 0, 14, 0.0F, true));
+
+		String1 = new ModelRenderer(this);
+		String1.setRotationPoint(7.0F, 23.0F, -6.5F);
+		setRotationAngle(String1, 0.0F, 0.0F, -0.7808F);
+		String1.cubeList.add(new ModelBox(String1, 0, 29, -1.0F, -0.5F, 0.0F, 19, 1, 0, 0.0F, true));
+
+		String2 = new ModelRenderer(this);
+		String2.setRotationPoint(7.0F, 23.0F, 6.5F);
+		setRotationAngle(String2, 0.0F, 0.0F, -0.7808F);
+		String2.cubeList.add(new ModelBox(String2, 0, 29, -1.0F, -0.5F, 0.0F, 19, 1, 0, 0.0F, true));
+
+		String3 = new ModelRenderer(this);
+		String3.setRotationPoint(6.5F, 23.0F, -7.0F);
+		setRotationAngle(String3, 0.0F, 1.5708F, -0.7808F);
+		String3.cubeList.add(new ModelBox(String3, 0, 29, -1.0F, -0.5F, 0.0F, 19, 1, 0, 0.0F, true));
+
+		String4 = new ModelRenderer(this);
+		String4.setRotationPoint(-6.5F, 23.0F, -7.0F);
+		setRotationAngle(String4, 0.0F, 1.5708F, -0.7808F);
+		String4.cubeList.add(new ModelBox(String4, 0, 29, -1.0F, -0.5F, 0.0F, 19, 1, 0, 0.0F, true));
+
+		String5 = new ModelRenderer(this);
+		String5.setRotationPoint(6.5F, 23.0F, 7.0F);
+		setRotationAngle(String5, 0.0F, -1.5708F, -0.7808F);
+		String5.cubeList.add(new ModelBox(String5, 0, 29, -1.0F, -0.5F, 0.0F, 19, 1, 0, 0.0F, true));
+
+		String6 = new ModelRenderer(this);
+		String6.setRotationPoint(-6.5F, 23.0F, 7.0F);
+		setRotationAngle(String6, 0.0F, -1.5708F, -0.7808F);
+		String6.cubeList.add(new ModelBox(String6, 0, 29, -1.0F, -0.5F, 0.0F, 19, 1, 0, 0.0F, true));
+
+		String7 = new ModelRenderer(this);
+		String7.setRotationPoint(-7.0F, 23.0F, 6.5F);
+		setRotationAngle(String7, 0.0F, 3.1416F, -0.7808F);
+		String7.cubeList.add(new ModelBox(String7, 0, 29, -1.0F, -0.5F, 0.0F, 19, 1, 0, 0.0F, true));
+
+		String8 = new ModelRenderer(this);
+		String8.setRotationPoint(-7.0F, 23.0F, -6.5F);
+		setRotationAngle(String8, 0.0F, 3.1416F, -0.7808F);
+		String8.cubeList.add(new ModelBox(String8, 0, 29, -1.0F, -0.5F, 0.0F, 19, 1, 0, 0.0F, true));
+	}
+
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		Center.render(f5);
+		Side1.render(f5);
+		Side2.render(f5);
+		Side3.render(f5);
+		Side4.render(f5);
+		Pillar1.render(f5);
+		Pillar2.render(f5);
+		Pillar3.render(f5);
+		Pillar4.render(f5);
+		Stick.render(f5);
+		Rock.render(f5);
+		Support1.render(f5);
+		Support2.render(f5);
+		Base.render(f5);
+		String1.render(f5);
+		String2.render(f5);
+		String3.render(f5);
+		String4.render(f5);
+		String5.render(f5);
+		String6.render(f5);
+		String7.render(f5);
+		String8.render(f5);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
 }
