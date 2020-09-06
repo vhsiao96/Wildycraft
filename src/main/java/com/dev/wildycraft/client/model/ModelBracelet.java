81aaf776a9509271f38c020a07ab91084814915f
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-09-06 19:07:52 -0400

    Updated Bracelet model
    Updated bracelet model to 1.12.2

--- src/main/java/com/dev/wildycraft/client/model/ModelBracelet.java
+++ src/main/java/com/dev/wildycraft/client/model/ModelBracelet.java
@@ -1,44 +1,34 @@
+// Made with Blockbench 3.6.6
+// Exported for Minecraft version 1.12
+// Paste this class into your mod and generate all required imports
 
-
-package wildycraft.client.model;
+package com.dev.wildycraft.client.model;
 
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
 
-public class ModelBracelet extends ModelBase
-{
-    public ModelRenderer Shape1;
-  
-  public ModelBracelet()
-  {
-    textureWidth = 32;
-    textureHeight = 32;
-    
-      Shape1 = new ModelRenderer(this, 0, 0);
-      Shape1.addBox(-3.5F, 5F, -2.5F, 5, 1, 5);
-      Shape1.setRotationPoint(-5F, 2F, 0F);
-      Shape1.setTextureSize(32, 32);
-      Shape1.mirror = true;
-      setRotation(Shape1, 0F, 0F, 0F);
-  }
-  
-  /*public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5);
-    Shape1.render(f5);
-  }*/
-  
-  public void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
-  /*public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5);
-  }*/
+public class ModelBracelet extends ModelBase {
+	private final ModelRenderer Shape1;
 
+	public ModelBracelet() {
+		textureWidth = 32;
+		textureHeight = 32;
+
+		Shape1 = new ModelRenderer(this);
+		Shape1.setRotationPoint(-5.0F, 2.0F, 0.0F);
+		Shape1.cubeList.add(new ModelBox(Shape1, 0, 0, -3.5F, 5.0F, -2.5F, 5, 1, 5, 0.0F, true));
+	}
+
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		Shape1.render(f5);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
 }
