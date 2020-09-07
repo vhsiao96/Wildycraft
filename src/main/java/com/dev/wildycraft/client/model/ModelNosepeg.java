e757f095288807c0d0ab200db6cd24eefa01c68b
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-09-06 20:50:35 -0400

    Updated NosePeg model
    Updated NosePeg model to 1.12.2

--- src/main/java/com/dev/wildycraft/client/model/ModelNosepeg.java
+++ src/main/java/com/dev/wildycraft/client/model/ModelNosepeg.java
@@ -1,50 +1,42 @@
-package wildycraft.client.model;
+// Made with Blockbench 3.6.6
+// Exported for Minecraft version 1.12
+// Paste this class into your mod and generate all required imports
 
-import net.minecraft.client.model.ModelBiped;
+package com.dev.wildycraft.client.model;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelNosepeg extends ModelBiped
-{
-  //fields
-    ModelRenderer LeftPeg;
-    ModelRenderer RightPeg;
-  
-  public ModelNosepeg()
-  {
-	  
-    textureWidth = 64;
-    textureHeight = 32;
-    
-      LeftPeg = new ModelRenderer(this, 0, 0);
-      LeftPeg.addBox(-3F, -7F, -2.5F, 1, 4, 1);
-      LeftPeg.setRotationPoint(0F, 0F, 0F);
-      LeftPeg.setTextureSize(64, 32);
-      LeftPeg.mirror = true;
-      setRotation(LeftPeg, 0.4461411F, -0.4461433F, 0F);
-      RightPeg = new ModelRenderer(this, 0, 0);
-      RightPeg.addBox(2F, -7F, -2.5F, 1, 4, 1);
-      RightPeg.setRotationPoint(0F, 0F, 0F);
-      RightPeg.setTextureSize(64, 32);
-      RightPeg.mirror = true;
-      setRotation(RightPeg, 0.4461433F, 0.4461411F, 0F);
-      RightPeg.mirror = false;
-      this.bipedHead.addChild(RightPeg);
-      this.bipedHead.addChild(LeftPeg);
-  }
-  
-  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-  }
-  
-  private void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
+public class ModelNosepeg extends ModelBase {
+	private final ModelRenderer LeftPeg;
+	private final ModelRenderer RightPeg;
 
+	public ModelNosepeg() {
+		textureWidth = 64;
+		textureHeight = 32;
+
+		LeftPeg = new ModelRenderer(this);
+		LeftPeg.setRotationPoint(0.0F, 0.0F, 0.0F);
+		setRotationAngle(LeftPeg, 0.4461F, -0.4461F, 0.0F);
+		LeftPeg.cubeList.add(new ModelBox(LeftPeg, 32, 22, -3.0F, -7.0F, -2.5F, 1, 4, 1, 0.0F, true));
+
+		RightPeg = new ModelRenderer(this);
+		RightPeg.setRotationPoint(0.0F, 0.0F, 0.0F);
+		setRotationAngle(RightPeg, 0.4461F, 0.4461F, 0.0F);
+		RightPeg.cubeList.add(new ModelBox(RightPeg, 32, 22, 2.0F, -7.0F, -2.5F, 1, 4, 1, 0.0F, false));
+	}
+
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		LeftPeg.render(f5);
+		RightPeg.render(f5);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
 }
