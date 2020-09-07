0d07cb3981beb3588146a17927c8ee600d65ab26
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-09-07 00:34:59 -0400

    Updated Updated Unicorn model to 1.12.2
    Updated Updated Unicorn model to 1.12.2

--- src/main/java/com/dev/wildycraft/client/model/ModelUnicorn.java
+++ src/main/java/com/dev/wildycraft/client/model/ModelUnicorn.java
@@ -1,169 +1,122 @@
-package wildycraft.client.model;
+// Made with Blockbench 3.6.6
+// Exported for Minecraft version 1.12
+// Paste this class into your mod and generate all required imports
+
+package com.dev.wildycraft.client.model;
 
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.MathHelper;
 
-public class ModelUnicorn extends ModelBase
-{
-  //fields
-    ModelRenderer Horn;
-    ModelRenderer Mane;
-    ModelRenderer tail;
-    ModelRenderer TailHair;
-    ModelRenderer body;
-    ModelRenderer leg1;
-    ModelRenderer leg2;
-    ModelRenderer leg3;
-    ModelRenderer leg4;
-    ModelRenderer Ear1;
-    ModelRenderer Ear2;
-    ModelRenderer Head;
-    ModelRenderer Neck;
-    ModelRenderer Nose;
-  
-  public ModelUnicorn()
-  {
-    textureWidth = 256;
-    textureHeight = 256;
-    
-      Horn = new ModelRenderer(this, 110, 0);
-      Horn.addBox(-0.4666667F, -12F, -7F, 1, 5, 1);
-      Horn.setRotationPoint(0F, 7F, -11F);
-      Horn.setTextureSize(256, 256);
-      Horn.mirror = true;
-      setRotation(Horn, 0.4537856F, 0F, 0F);
-      Mane = new ModelRenderer(this, 120, 16);
-      Mane.addBox(-1F, -4F, -7F, 2, 4, 15);
-      Mane.setRotationPoint(0F, 7F, -11F);
-      Mane.setTextureSize(256, 256);
-      Mane.mirror = true;
-      setRotation(Mane, -0.6283185F, 0F, 0F);
-      tail = new ModelRenderer(this, 31, 0);
-      tail.addBox(-0.5F, 0F, 0F, 1, 1, 4);
-      tail.setRotationPoint(0F, 7.5F, 10F);
-      tail.setTextureSize(256, 256);
-      tail.mirror = true;
-      setRotation(tail, -1.047198F, 0F, 0F);
-      TailHair = new ModelRenderer(this, 0, 0);
-      TailHair.addBox(-1.5F, -1F, 2F, 3, 3, 7);
-      TailHair.setRotationPoint(0F, 7.5F, 10F);
-      TailHair.setTextureSize(256, 256);
-      TailHair.mirror = true;
-      setRotation(TailHair, -1.047198F, 0F, 0F);
-      body = new ModelRenderer(this, 53, 4);
-      body.addBox(-5F, -10F, -9F, 8, 21, 9);
-      body.setRotationPoint(1F, 7F, -1F);
-      body.setTextureSize(256, 256);
-      body.mirror = true;
-      setRotation(body, 1.570796F, 0F, 0F);
-      leg1 = new ModelRenderer(this, 0, 16);
-      leg1.addBox(-1.5F, 0F, -2F, 3, 12, 4);
-      leg1.setRotationPoint(-3F, 12F, 7F);
-      leg1.setTextureSize(256, 256);
-      leg1.mirror = true;
-      setRotation(leg1, 0F, 0F, 0F);
-      leg2 = new ModelRenderer(this, 0, 16);
-      leg2.addBox(-1.5F, 0F, -2F, 3, 12, 4);
-      leg2.setRotationPoint(3F, 12F, 7F);
-      leg2.setTextureSize(256, 256);
-      leg2.mirror = true;
-      setRotation(leg2, 0F, 0F, 0F);
-      leg3 = new ModelRenderer(this, 0, 16);
-      leg3.addBox(-1.5F, 0F, -2F, 3, 12, 4);
-      leg3.setRotationPoint(-3F, 12F, -9F);
-      leg3.setTextureSize(256, 256);
-      leg3.mirror = true;
-      setRotation(leg3, 0F, 0F, 0F);
-      leg4 = new ModelRenderer(this, 0, 16);
-      leg4.addBox(-1.5F, 0F, -2F, 3, 12, 4);
-      leg4.setRotationPoint(3F, 12F, -9F);
-      leg4.setTextureSize(256, 256);
-      leg4.mirror = true;
-      setRotation(leg4, 0F, 0F, 0F);
-      Ear1 = new ModelRenderer(this, 102, 0);
-      Ear1.addBox(-2F, -9F, -1F, 1, 2, 1);
-      Ear1.setRotationPoint(0F, 7F, -11F);
-      Ear1.setTextureSize(256, 256);
-      Ear1.mirror = true;
-      setRotation(Ear1, 0.4537856F, 0F, 0F);
-      Ear2 = new ModelRenderer(this, 96, 0);
-      Ear2.addBox(1F, -9F, -1F, 1, 2, 1);
-      Ear2.setRotationPoint(0F, 7F, -11F);
-      Ear2.setTextureSize(256, 256);
-      Ear2.mirror = true;
-      setRotation(Ear2, 0.4537856F, 0F, 0F);
-      Head = new ModelRenderer(this, 128, 0);
-      Head.addBox(-2F, -7F, -8F, 4, 4, 8);
-      Head.setRotationPoint(0F, 7F, -11F);
-      Head.setTextureSize(256, 256);
-      Head.mirror = true;
-      setRotation(Head, 0.4537856F, 0F, 0F);
-      Neck = new ModelRenderer(this, 90, 20);
-      Neck.addBox(-2F, -3F, -5F, 4, 4, 10);
-      Neck.setRotationPoint(0F, 8F, -11F);
-      Neck.setTextureSize(256, 256);
-      Neck.mirror = true;
-      setRotation(Neck, -0.6283185F, 0F, 0F);
-      Nose = new ModelRenderer(this, 117, 0);
-      Nose.addBox(-1.5F, -6.7F, -8.7F, 3, 2, 1);
-      Nose.setRotationPoint(0F, 7F, -11F);
-      Nose.setTextureSize(256, 256);
-      Nose.mirror = true;
-      setRotation(Nose, 0.4537856F, 0F, 0F);
-  }
-  
-  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-    Horn.render(f5);
-    Mane.render(f5);
-    tail.render(f5);
-    TailHair.render(f5);
-    body.render(f5);
-    leg1.render(f5);
-    leg2.render(f5);
-    leg3.render(f5);
-    leg4.render(f5);
-    Ear1.render(f5);
-    Ear2.render(f5);
-    Head.render(f5);
-    Neck.render(f5);
-    Nose.render(f5);
-  }
-  
-  private void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
-  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
-  {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-    
-    this.Head.rotateAngleX = 0.4537856F + f3 / (180F / (float)Math.PI)/2;
-    this.Head.rotateAngleY = f4 / (180F / (float)Math.PI);
-    this.Neck.rotateAngleX = -0.6283185F + f3 / (180F / (float)Math.PI)/2;
-    this.Neck.rotateAngleY = f4 / (180F / (float)Math.PI);
-    this.Nose.rotateAngleX = 0.4537856F + f3 / (180F / (float)Math.PI)/2;
-    this.Nose.rotateAngleY = f4 / (180F / (float)Math.PI);
-    this.Ear1.rotateAngleX = 0.4537856F + f3 / (180F / (float)Math.PI)/2;
-    this.Ear1.rotateAngleY = f4 / (180F / (float)Math.PI);
-    this.Ear2.rotateAngleX = 0.4537856F + f3 / (180F / (float)Math.PI)/2;
-    this.Ear2.rotateAngleY = f4 / (180F / (float)Math.PI);
-    this.Mane.rotateAngleX = -0.6283185F + f3 / (180F / (float)Math.PI)/2;
-    this.Mane.rotateAngleY = f4 / (180F / (float)Math.PI);
-    this.Horn.rotateAngleX = 0.4537856F + f3 / (180F / (float)Math.PI)/2;
-    this.Horn.rotateAngleY = f4 / (180F / (float)Math.PI);
-    
-    this.leg1.rotateAngleX = MathHelper.cos(f * 0.6662F) * 1.4F * f1;
-    this.leg2.rotateAngleX = MathHelper.cos(f * 0.6662F + (float)Math.PI) * 1.4F * f1;
-    this.leg3.rotateAngleX = MathHelper.cos(f * 0.6662F + (float)Math.PI) * 1.4F * f1;
-    this.leg4.rotateAngleX = MathHelper.cos(f * 0.6662F) * 1.4F * f1;
-  }
+public class ModelUnicorn extends ModelBase {
+	private final ModelRenderer Horn;
+	private final ModelRenderer Mane;
+	private final ModelRenderer tail;
+	private final ModelRenderer TailHair;
+	private final ModelRenderer body;
+	private final ModelRenderer leg1;
+	private final ModelRenderer leg2;
+	private final ModelRenderer leg3;
+	private final ModelRenderer leg4;
+	private final ModelRenderer Ear1;
+	private final ModelRenderer Ear2;
+	private final ModelRenderer Head;
+	private final ModelRenderer Neck;
+	private final ModelRenderer Nose;
 
+	public ModelUnicorn() {
+		textureWidth = 256;
+		textureHeight = 256;
+
+		Horn = new ModelRenderer(this);
+		Horn.setRotationPoint(0.0F, 7.0F, -11.0F);
+		setRotationAngle(Horn, 0.4538F, 0.0F, 0.0F);
+		Horn.cubeList.add(new ModelBox(Horn, 110, 0, -0.4667F, -12.0F, -7.0F, 1, 5, 1, 0.0F, true));
+
+		Mane = new ModelRenderer(this);
+		Mane.setRotationPoint(0.0F, 7.0F, -11.0F);
+		setRotationAngle(Mane, -0.6283F, 0.0F, 0.0F);
+		Mane.cubeList.add(new ModelBox(Mane, 120, 16, -1.0F, -4.0F, -7.0F, 2, 4, 15, 0.0F, true));
+
+		tail = new ModelRenderer(this);
+		tail.setRotationPoint(0.0F, 7.5F, 10.0F);
+		setRotationAngle(tail, -1.0472F, 0.0F, 0.0F);
+		tail.cubeList.add(new ModelBox(tail, 31, 0, -0.5F, 0.0F, 0.0F, 1, 1, 4, 0.0F, true));
+
+		TailHair = new ModelRenderer(this);
+		TailHair.setRotationPoint(0.0F, 7.5F, 10.0F);
+		setRotationAngle(TailHair, -1.0472F, 0.0F, 0.0F);
+		TailHair.cubeList.add(new ModelBox(TailHair, 0, 0, -1.5F, -1.0F, 2.0F, 3, 3, 7, 0.0F, true));
+
+		body = new ModelRenderer(this);
+		body.setRotationPoint(1.0F, 7.0F, -1.0F);
+		setRotationAngle(body, 1.5708F, 0.0F, 0.0F);
+		body.cubeList.add(new ModelBox(body, 53, 4, -5.0F, -10.0F, -9.0F, 8, 21, 9, 0.0F, true));
+
+		leg1 = new ModelRenderer(this);
+		leg1.setRotationPoint(-3.0F, 12.0F, 7.0F);
+		leg1.cubeList.add(new ModelBox(leg1, 0, 16, -1.5F, 0.0F, -2.0F, 3, 12, 4, 0.0F, true));
+
+		leg2 = new ModelRenderer(this);
+		leg2.setRotationPoint(3.0F, 12.0F, 7.0F);
+		leg2.cubeList.add(new ModelBox(leg2, 0, 16, -1.5F, 0.0F, -2.0F, 3, 12, 4, 0.0F, true));
+
+		leg3 = new ModelRenderer(this);
+		leg3.setRotationPoint(-3.0F, 12.0F, -9.0F);
+		leg3.cubeList.add(new ModelBox(leg3, 0, 16, -1.5F, 0.0F, -2.0F, 3, 12, 4, 0.0F, true));
+
+		leg4 = new ModelRenderer(this);
+		leg4.setRotationPoint(3.0F, 12.0F, -9.0F);
+		leg4.cubeList.add(new ModelBox(leg4, 0, 16, -1.5F, 0.0F, -2.0F, 3, 12, 4, 0.0F, true));
+
+		Ear1 = new ModelRenderer(this);
+		Ear1.setRotationPoint(0.0F, 7.0F, -11.0F);
+		setRotationAngle(Ear1, 0.4538F, 0.0F, 0.0F);
+		Ear1.cubeList.add(new ModelBox(Ear1, 102, 0, -2.0F, -9.0F, -1.0F, 1, 2, 1, 0.0F, true));
+
+		Ear2 = new ModelRenderer(this);
+		Ear2.setRotationPoint(0.0F, 7.0F, -11.0F);
+		setRotationAngle(Ear2, 0.4538F, 0.0F, 0.0F);
+		Ear2.cubeList.add(new ModelBox(Ear2, 96, 0, 1.0F, -9.0F, -1.0F, 1, 2, 1, 0.0F, true));
+
+		Head = new ModelRenderer(this);
+		Head.setRotationPoint(0.0F, 7.0F, -11.0F);
+		setRotationAngle(Head, 0.4538F, 0.0F, 0.0F);
+		Head.cubeList.add(new ModelBox(Head, 128, 0, -2.0F, -7.0F, -8.0F, 4, 4, 8, 0.0F, true));
+
+		Neck = new ModelRenderer(this);
+		Neck.setRotationPoint(0.0F, 8.0F, -11.0F);
+		setRotationAngle(Neck, -0.6283F, 0.0F, 0.0F);
+		Neck.cubeList.add(new ModelBox(Neck, 90, 20, -2.0F, -3.0F, -5.0F, 4, 4, 10, 0.0F, true));
+
+		Nose = new ModelRenderer(this);
+		Nose.setRotationPoint(0.0F, 7.0F, -11.0F);
+		setRotationAngle(Nose, 0.4538F, 0.0F, 0.0F);
+		Nose.cubeList.add(new ModelBox(Nose, 117, 0, -1.5F, -6.7F, -8.7F, 3, 2, 1, 0.0F, true));
+	}
+
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		Horn.render(f5);
+		Mane.render(f5);
+		tail.render(f5);
+		TailHair.render(f5);
+		body.render(f5);
+		leg1.render(f5);
+		leg2.render(f5);
+		leg3.render(f5);
+		leg4.render(f5);
+		Ear1.render(f5);
+		Ear2.render(f5);
+		Head.render(f5);
+		Neck.render(f5);
+		Nose.render(f5);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
 }
