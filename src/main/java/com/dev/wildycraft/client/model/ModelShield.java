4ff063c0071d5a2bee4ebd6cd71331aa4e1e8f17
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-09-07 01:15:20 -0400

    Updated Shield model
    Updated Shield model to 1.12.2

--- src/main/java/com/dev/wildycraft/client/model/ModelShield.java
+++ src/main/java/com/dev/wildycraft/client/model/ModelShield.java
@@ -1,52 +1,40 @@
+// Made with Blockbench 3.6.6
+// Exported for Minecraft version 1.12
+// Paste this class into your mod and generate all required imports
 
-package wildycraft.client.model;
+package com.dev.wildycraft.client.model;
 
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
 
-public class ModelShield extends ModelBase
-{
-  //fields
-    public ModelRenderer shieldP1;
-    public ModelRenderer shieldP2;
-  
-  public ModelShield()
-  {
-    textureWidth = 64;
-    textureHeight = 64;
-    
-      shieldP1 = new ModelRenderer(this, 0, 0);
-      shieldP1.addBox(3F, 3F, -8F, 1, 11, 16);
-      shieldP1.setRotationPoint(5F, 2F, 0F);
-      shieldP1.setTextureSize(64, 64);
-      shieldP1.mirror = true;
-      setRotation(shieldP1, 0F, 0F, 0F);
-      shieldP2 = new ModelRenderer(this, 0, 32);
-      shieldP2.addBox(3.5F, 4F, -8F, 1, 9, 16);
-      shieldP2.setRotationPoint(5F, 2F, 0F);
-      shieldP2.setTextureSize(64, 64);
-      shieldP2.mirror = true;
-      setRotation(shieldP2, 0F, 0F, 0F);
-  }
-  
-  /*public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.render(entity, f, f1, f2, f3, f4, f5);
-    setRotationAngles(f, f1, f2, f3, f4, f5);
-    Shape1.render(f5);
-    Shape2.render(f5);
-  }*/
-  
-  public void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
-  /*public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    super.setRotationAngles(f, f1, f2, f3, f4, f5);
-  }*/
+public class ModelShield extends ModelBase {
+	private final ModelRenderer shieldP1;
+	private final ModelRenderer shieldP2;
 
+	public ModelShield() {
+		textureWidth = 64;
+		textureHeight = 64;
+
+		shieldP1 = new ModelRenderer(this);
+		shieldP1.setRotationPoint(5.0F, 2.0F, 0.0F);
+		shieldP1.cubeList.add(new ModelBox(shieldP1, 0, 0, 3.0F, 3.0F, -8.0F, 1, 11, 16, 0.0F, true));
+
+		shieldP2 = new ModelRenderer(this);
+		shieldP2.setRotationPoint(5.0F, 2.0F, 0.0F);
+		shieldP2.cubeList.add(new ModelBox(shieldP2, 0, 32, 3.5F, 4.0F, -8.0F, 1, 9, 16, 0.0F, true));
+	}
+
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		shieldP1.render(f5);
+		shieldP2.render(f5);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
 }
