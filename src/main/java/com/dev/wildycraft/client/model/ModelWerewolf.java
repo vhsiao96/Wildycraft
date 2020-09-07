7918f44858926c9d8de7d18acdc7250f6ccf0488
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-09-07 01:43:57 -0400

    Updated the Werewolf model
    Updated the Werewolf model to 1.12.2

--- src/main/java/com/dev/wildycraft/client/model/ModelWerewolf.java
+++ src/main/java/com/dev/wildycraft/client/model/ModelWerewolf.java
@@ -1,229 +1,175 @@
-package wildycraft.client.model;
+// Made with Blockbench 3.6.6
+// Exported for Minecraft version 1.12
+// Paste this class into your mod and generate all required imports
 
-import org.lwjgl.opengl.GL11;
-
-import wildycraft.entity.EntityWerewolf;
+package com.dev.wildycraft.client.model;
 
 import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelWerewolf extends ModelBiped
-{
-	
-    ModelRenderer head;
-    ModelRenderer body;
-    ModelRenderer rightarm;
-    ModelRenderer leftarm;
-    ModelRenderer rightleg;
-    ModelRenderer leftleg;
-    ModelRenderer snout;
-    ModelRenderer leftear;
-    ModelRenderer rightear;
-    ModelRenderer tail;
-    ModelRenderer rightthumb;
-    ModelRenderer leftthumb;
-    ModelRenderer rightclaw1;
-    ModelRenderer rightclaw2;
-    ModelRenderer rightclaw3;
-    ModelRenderer leftclaw1;
-    ModelRenderer leftclaw2;
-    ModelRenderer leftclaw3;
-    ModelRenderer upperbody;
-    ModelRenderer lowerjaw;
-    ModelRenderer backjaw;
-  
-  public ModelWerewolf()
-  {
-	  super(0.0F, 0.0F, 128, 64);
-	  textureWidth = 128;
-	  textureHeight = 64;
-    
-      head = new ModelRenderer(this, 96, 16);
-      head.addBox(-4F, -8F, -7F, 8, 8, 8);
-      head.setRotationPoint(0F, -1F, 0F);
-      head.setTextureSize(128, 64);
-      head.mirror = true;
-      setRotation(head, 0.0698132F, 0F, 0F);
-      body = new ModelRenderer(this, 72, 47);
-      body.addBox(-4F, -3F, -2F, 8, 13, 4);
-      body.setRotationPoint(0F, 0F, 0F);
-      body.setTextureSize(128, 64);
-      body.mirror = true;
-      setRotation(body, 0.0349066F, 0F, 0F);
-      rightarm = new ModelRenderer(this, 96, 46);
-      rightarm.addBox(-3F, -2F, -2F, 4, 14, 4);
-      rightarm.setRotationPoint(-6F, -1F, 0F);
-      rightarm.setTextureSize(128, 64);
-      rightarm.mirror = true;
-      setRotation(rightarm, 0F, 0F, 0.0872665F);
-      leftarm = new ModelRenderer(this, 96, 46);
-      leftarm.addBox(-1F, -2F, -2F, 4, 14, 4);
-      leftarm.setRotationPoint(6F, -1F, 0F);
-      leftarm.setTextureSize(128, 64);
-      leftarm.mirror = true;
-      setRotation(leftarm, 0F, 0F, -0.0872665F);
-      rightleg = new ModelRenderer(this, 112, 46);
-      rightleg.addBox(-2F, 0F, -1.7F, 4, 14, 4);
-      rightleg.setRotationPoint(-2F, 10F, 0F);
-      rightleg.setTextureSize(128, 64);
-      rightleg.mirror = true;
-      setRotation(rightleg, 0F, 0F, 0.0349066F);
-      leftleg = new ModelRenderer(this, 112, 46);
-      leftleg.addBox(-2F, 0F, -1.7F, 4, 14, 4);
-      leftleg.setRotationPoint(2F, 10F, 0F);
-      leftleg.setTextureSize(128, 64);
-      leftleg.mirror = true;
-      setRotation(leftleg, 0F, 0F, -0.0349066F);
-      snout = new ModelRenderer(this, 81, 0);
-      snout.addBox(-2F, -4F, -12F, 4, 2, 6);
-      snout.setRotationPoint(0F, -1F, 0F);
-      snout.setTextureSize(128, 64);
-      snout.mirror = true;
-      setRotation(snout, 0.0698132F, 0F, 0F);
-      leftear = new ModelRenderer(this, 86, 24);
-      leftear.addBox(2F, -11F, -2F, 2, 3, 2);
-      leftear.setRotationPoint(0F, -1F, 0F);
-      leftear.setTextureSize(128, 64);
-      leftear.mirror = true;
-      setRotation(leftear, 0.0698132F, 0F, 0F);
-      rightear = new ModelRenderer(this, 86, 24);
-      rightear.addBox(-4F, -11F, -2F, 2, 3, 2);
-      rightear.setRotationPoint(0F, -1F, 0F);
-      rightear.setTextureSize(128, 64);
-      rightear.mirror = true;
-      setRotation(rightear, 0.0698132F, 0F, 0F);
-      tail = new ModelRenderer(this, 80, 16);
-      tail.addBox(-1F, -1F, 0F, 2, 2, 5);
-      tail.setRotationPoint(0F, 9F, 1.5F);
-      tail.setTextureSize(128, 64);
-      tail.mirror = true;
-      setRotation(tail, 0.4886922F, 0F, 0F);
-      rightthumb = new ModelRenderer(this, 72, 20);
-      rightthumb.addBox(-3.5F, 9F, 5F, 0, 2, 1);
-      rightthumb.setRotationPoint(-5F, -1F, 0F);
-      rightthumb.setTextureSize(128, 64);
-      rightthumb.mirror = true;
-      setRotation(rightthumb, -0.6806784F, 0F, 0.0872665F);
-      leftthumb = new ModelRenderer(this, 72, 20);
-      leftthumb.addBox(2.5F, 9F, 5F, 0, 2, 1);
-      leftthumb.setRotationPoint(6F, -1F, 0F);
-      leftthumb.setTextureSize(128, 64);
-      leftthumb.mirror = true;
-      setRotation(leftthumb, -0.6806784F, 0F, -0.0872665F);
-      rightclaw1 = new ModelRenderer(this, 72, 31);
-      rightclaw1.addBox(-4F, 11F, 1F, 1, 6, 0);
-      rightclaw1.setRotationPoint(-5F, -1F, 0F);
-      rightclaw1.setTextureSize(128, 64);
-      rightclaw1.mirror = true;
-      setRotation(rightclaw1, 0F, 0F, 0.0872665F);
-      rightclaw2 = new ModelRenderer(this, 72, 31);
-      rightclaw2.addBox(-4F, 11F, 0F, 1, 6, 0);
-      rightclaw2.setRotationPoint(-5F, -1F, 0F);
-      rightclaw2.setTextureSize(128, 64);
-      rightclaw2.mirror = true;
-      setRotation(rightclaw2, 0F, 0F, 0.0872665F);
-      rightclaw3 = new ModelRenderer(this, 72, 31);
-      rightclaw3.addBox(-4F, 11F, -1F, 1, 6, 0);
-      rightclaw3.setRotationPoint(-5F, -1F, 0F);
-      rightclaw3.setTextureSize(128, 64);
-      rightclaw3.mirror = true;
-      setRotation(rightclaw3, 0F, 0F, 0.0872665F);
-      leftclaw1 = new ModelRenderer(this, 72, 31);
-      leftclaw1.addBox(3F, 11F, 1F, 1, 6, 0);
-      leftclaw1.setRotationPoint(5F, -1F, 0F);
-      leftclaw1.setTextureSize(128, 64);
-      leftclaw1.mirror = true;
-      setRotation(leftclaw1, 0F, 0F, -0.0872665F);
-      leftclaw2 = new ModelRenderer(this, 72, 31);
-      leftclaw2.addBox(3F, 11F, 0F, 1, 6, 0);
-      leftclaw2.setRotationPoint(5F, -1F, 0F);
-      leftclaw2.setTextureSize(128, 64);
-      leftclaw2.mirror = true;
-      setRotation(leftclaw2, 0F, 0F, -0.0872665F);
-      leftclaw3 = new ModelRenderer(this, 72, 31);
-      leftclaw3.addBox(3F, 11F, -1F, 1, 6, 0);
-      leftclaw3.setRotationPoint(5F, -1F, 0F);
-      leftclaw3.setTextureSize(128, 64);
-      leftclaw3.mirror = true;
-      setRotation(leftclaw3, 0F, 0F, -0.0872665F);
-      upperbody = new ModelRenderer(this, 80, 32);
-      upperbody.addBox(-9F, -4F, -3F, 18, 8, 6);
-      upperbody.setRotationPoint(0F, 0F, 0F);
-      upperbody.setTextureSize(128, 64);
-      upperbody.mirror = true;
-      setRotation(upperbody, 0.1047198F, 0F, 0F);
-      lowerjaw = new ModelRenderer(this, 104, 0);
-      lowerjaw.addBox(-2F, -1F, -12F, 4, 1, 6);
-      lowerjaw.setRotationPoint(0F, -1F, 0F);
-      lowerjaw.setTextureSize(128, 64);
-      lowerjaw.mirror = true;
-      setRotation(lowerjaw, 0.0698132F, 0F, 0F);
-      backjaw = new ModelRenderer(this, 105, 9);
-      backjaw.addBox(-2F, -5F, -7.5F, 4, 2, 2);
-      backjaw.setRotationPoint(0F, -1F, 0F);
-      backjaw.setTextureSize(128, 64);
-      backjaw.mirror = true;
-      setRotation(backjaw, 0.3839724F, 0F, 0F);
-  }
-  
-  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-  {
-    setRotationAngles( f, f1, f2, f3, f4, f5, entity);
-   
-    //GL11.glEnable(GL11.GL_BLEND);
-    //GL11.glColor4f(1.0f, 1.0f, 1.0f, 0.26f);
-    if(((EntityWerewolf)entity).getType() == 1){
-    head.render(f5);
-    body.render(f5);
-    rightarm.render(f5);
-    leftarm.render(f5);
-    rightleg.render(f5);
-    leftleg.render(f5);
-    snout.render(f5);
-    leftear.render(f5);
-    rightear.render(f5);
-    tail.render(f5);
-    rightthumb.render(f5);
-    leftthumb.render(f5);
-    rightclaw1.render(f5);
-    rightclaw2.render(f5);
-    rightclaw3.render(f5);
-    leftclaw1.render(f5);
-    leftclaw2.render(f5);
-    leftclaw3.render(f5);
-    upperbody.render(f5);
-    lowerjaw.render(f5);
-    backjaw.render(f5);
-    } else {
-    	super.render(entity, f, f1, f2, f3, f4, f5);
-    }
-    //GL11.glDisable(GL11.GL_BLEND);
-  }
-  
-  private void setRotation(ModelRenderer model, float x, float y, float z)
-  {
-    model.rotateAngleX = x;
-    model.rotateAngleY = y;
-    model.rotateAngleZ = z;
-  }
-  
-  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
-  {
-	  super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-	  this.rightleg.rotateAngleX = this.bipedRightLeg.rotateAngleX;
-	  this.leftleg.rotateAngleX = this.bipedLeftLeg.rotateAngleX;
-	  this.rightarm.rotateAngleX = this.bipedRightArm.rotateAngleX;
-	  this.leftarm.rotateAngleX = this.bipedLeftArm.rotateAngleX;
-	  this.rightclaw1.rotateAngleX = this.bipedRightArm.rotateAngleX;
-	  this.leftclaw1.rotateAngleX = this.bipedLeftArm.rotateAngleX;
-	  this.rightclaw2.rotateAngleX = this.bipedRightArm.rotateAngleX;
-	  this.leftclaw2.rotateAngleX = this.bipedLeftArm.rotateAngleX;
-	  this.rightclaw3.rotateAngleX = this.bipedRightArm.rotateAngleX;
-	  this.leftclaw3.rotateAngleX = this.bipedLeftArm.rotateAngleX;
-	  this.rightthumb.rotateAngleX = -0.6806784F + this.bipedRightArm.rotateAngleX;
-	  this.leftthumb.rotateAngleX = -0.6806784F + this.bipedLeftArm.rotateAngleX;
-  }
+public class ModelWerewolf extends ModelBase {
+	private final ModelRenderer head;
+	private final ModelRenderer body;
+	private final ModelRenderer rightarm;
+	private final ModelRenderer leftarm;
+	private final ModelRenderer rightleg;
+	private final ModelRenderer leftleg;
+	private final ModelRenderer snout;
+	private final ModelRenderer leftear;
+	private final ModelRenderer rightear;
+	private final ModelRenderer tail;
+	private final ModelRenderer rightthumb;
+	private final ModelRenderer leftthumb;
+	private final ModelRenderer rightclaw1;
+	private final ModelRenderer rightclaw2;
+	private final ModelRenderer rightclaw3;
+	private final ModelRenderer leftclaw1;
+	private final ModelRenderer leftclaw2;
+	private final ModelRenderer leftclaw3;
+	private final ModelRenderer upperbody;
+	private final ModelRenderer lowerjaw;
+	private final ModelRenderer backjaw;
 
+	public ModelWerewolf() {
+		textureWidth = 128;
+		textureHeight = 64;
+
+		head = new ModelRenderer(this);
+		head.setRotationPoint(0.0F, -1.0F, 0.0F);
+		setRotationAngle(head, 0.0698F, 0.0F, 0.0F);
+		head.cubeList.add(new ModelBox(head, 96, 16, -4.0F, -8.0F, -7.0F, 8, 8, 8, 0.0F, true));
+
+		body = new ModelRenderer(this);
+		body.setRotationPoint(0.0F, 0.0F, 0.0F);
+		setRotationAngle(body, 0.0349F, 0.0F, 0.0F);
+		body.cubeList.add(new ModelBox(body, 72, 47, -4.0F, -3.0F, -2.0F, 8, 13, 4, 0.0F, true));
+
+		rightarm = new ModelRenderer(this);
+		rightarm.setRotationPoint(-6.0F, -1.0F, 0.0F);
+		setRotationAngle(rightarm, 0.0F, 0.0F, 0.0873F);
+		rightarm.cubeList.add(new ModelBox(rightarm, 96, 46, -3.0F, -2.0F, -2.0F, 4, 14, 4, 0.0F, true));
+
+		leftarm = new ModelRenderer(this);
+		leftarm.setRotationPoint(6.0F, -1.0F, 0.0F);
+		setRotationAngle(leftarm, 0.0F, 0.0F, -0.0873F);
+		leftarm.cubeList.add(new ModelBox(leftarm, 96, 46, -1.0F, -2.0F, -2.0F, 4, 14, 4, 0.0F, true));
+
+		rightleg = new ModelRenderer(this);
+		rightleg.setRotationPoint(-2.0F, 10.0F, 0.0F);
+		setRotationAngle(rightleg, 0.0F, 0.0F, 0.0349F);
+		rightleg.cubeList.add(new ModelBox(rightleg, 112, 46, -2.0F, 0.0F, -1.7F, 4, 14, 4, 0.0F, true));
+
+		leftleg = new ModelRenderer(this);
+		leftleg.setRotationPoint(2.0F, 10.0F, 0.0F);
+		setRotationAngle(leftleg, 0.0F, 0.0F, -0.0349F);
+		leftleg.cubeList.add(new ModelBox(leftleg, 112, 46, -2.0F, 0.0F, -1.7F, 4, 14, 4, 0.0F, true));
+
+		snout = new ModelRenderer(this);
+		snout.setRotationPoint(0.0F, -1.0F, 0.0F);
+		setRotationAngle(snout, 0.0698F, 0.0F, 0.0F);
+		snout.cubeList.add(new ModelBox(snout, 81, 0, -2.0F, -4.0F, -12.0F, 4, 2, 6, 0.0F, true));
+
+		leftear = new ModelRenderer(this);
+		leftear.setRotationPoint(0.0F, -1.0F, 0.0F);
+		setRotationAngle(leftear, 0.0698F, 0.0F, 0.0F);
+		leftear.cubeList.add(new ModelBox(leftear, 86, 24, 2.0F, -11.0F, -2.0F, 2, 3, 2, 0.0F, true));
+
+		rightear = new ModelRenderer(this);
+		rightear.setRotationPoint(0.0F, -1.0F, 0.0F);
+		setRotationAngle(rightear, 0.0698F, 0.0F, 0.0F);
+		rightear.cubeList.add(new ModelBox(rightear, 86, 24, -4.0F, -11.0F, -2.0F, 2, 3, 2, 0.0F, true));
+
+		tail = new ModelRenderer(this);
+		tail.setRotationPoint(0.0F, 9.0F, 1.5F);
+		setRotationAngle(tail, 0.4887F, 0.0F, 0.0F);
+		tail.cubeList.add(new ModelBox(tail, 80, 16, -1.0F, -1.0F, 0.0F, 2, 2, 5, 0.0F, true));
+
+		rightthumb = new ModelRenderer(this);
+		rightthumb.setRotationPoint(-5.0F, -1.0F, 0.0F);
+		setRotationAngle(rightthumb, -0.6807F, 0.0F, 0.0873F);
+		rightthumb.cubeList.add(new ModelBox(rightthumb, 72, 20, -3.5F, 9.0F, 5.0F, 0, 2, 1, 0.0F, true));
+
+		leftthumb = new ModelRenderer(this);
+		leftthumb.setRotationPoint(6.0F, -1.0F, 0.0F);
+		setRotationAngle(leftthumb, -0.6807F, 0.0F, -0.0873F);
+		leftthumb.cubeList.add(new ModelBox(leftthumb, 72, 20, 2.5F, 9.0F, 5.0F, 0, 2, 1, 0.0F, true));
+
+		rightclaw1 = new ModelRenderer(this);
+		rightclaw1.setRotationPoint(-5.0F, -1.0F, 0.0F);
+		setRotationAngle(rightclaw1, 0.0F, 0.0F, 0.0873F);
+		rightclaw1.cubeList.add(new ModelBox(rightclaw1, 72, 31, -4.0F, 11.0F, 1.0F, 1, 6, 0, 0.0F, true));
+
+		rightclaw2 = new ModelRenderer(this);
+		rightclaw2.setRotationPoint(-5.0F, -1.0F, 0.0F);
+		setRotationAngle(rightclaw2, 0.0F, 0.0F, 0.0873F);
+		rightclaw2.cubeList.add(new ModelBox(rightclaw2, 72, 31, -4.0F, 11.0F, 0.0F, 1, 6, 0, 0.0F, true));
+
+		rightclaw3 = new ModelRenderer(this);
+		rightclaw3.setRotationPoint(-5.0F, -1.0F, 0.0F);
+		setRotationAngle(rightclaw3, 0.0F, 0.0F, 0.0873F);
+		rightclaw3.cubeList.add(new ModelBox(rightclaw3, 72, 31, -4.0F, 11.0F, -1.0F, 1, 6, 0, 0.0F, true));
+
+		leftclaw1 = new ModelRenderer(this);
+		leftclaw1.setRotationPoint(5.0F, -1.0F, 0.0F);
+		setRotationAngle(leftclaw1, 0.0F, 0.0F, -0.0873F);
+		leftclaw1.cubeList.add(new ModelBox(leftclaw1, 72, 31, 3.0F, 11.0F, 1.0F, 1, 6, 0, 0.0F, true));
+
+		leftclaw2 = new ModelRenderer(this);
+		leftclaw2.setRotationPoint(5.0F, -1.0F, 0.0F);
+		setRotationAngle(leftclaw2, 0.0F, 0.0F, -0.0873F);
+		leftclaw2.cubeList.add(new ModelBox(leftclaw2, 72, 31, 3.0F, 11.0F, 0.0F, 1, 6, 0, 0.0F, true));
+
+		leftclaw3 = new ModelRenderer(this);
+		leftclaw3.setRotationPoint(5.0F, -1.0F, 0.0F);
+		setRotationAngle(leftclaw3, 0.0F, 0.0F, -0.0873F);
+		leftclaw3.cubeList.add(new ModelBox(leftclaw3, 72, 31, 3.0F, 11.0F, -1.0F, 1, 6, 0, 0.0F, true));
+
+		upperbody = new ModelRenderer(this);
+		upperbody.setRotationPoint(0.0F, 0.0F, 0.0F);
+		setRotationAngle(upperbody, 0.1047F, 0.0F, 0.0F);
+		upperbody.cubeList.add(new ModelBox(upperbody, 80, 32, -9.0F, -4.0F, -3.0F, 18, 8, 6, 0.0F, true));
+
+		lowerjaw = new ModelRenderer(this);
+		lowerjaw.setRotationPoint(0.0F, -1.0F, 0.0F);
+		setRotationAngle(lowerjaw, 0.0698F, 0.0F, 0.0F);
+		lowerjaw.cubeList.add(new ModelBox(lowerjaw, 104, 0, -2.0F, -1.0F, -12.0F, 4, 1, 6, 0.0F, true));
+
+		backjaw = new ModelRenderer(this);
+		backjaw.setRotationPoint(0.0F, -1.0F, 0.0F);
+		setRotationAngle(backjaw, 0.384F, 0.0F, 0.0F);
+		backjaw.cubeList.add(new ModelBox(backjaw, 105, 9, -2.0F, -5.0F, -7.5F, 4, 2, 2, 0.0F, true));
+	}
+
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		head.render(f5);
+		body.render(f5);
+		rightarm.render(f5);
+		leftarm.render(f5);
+		rightleg.render(f5);
+		leftleg.render(f5);
+		snout.render(f5);
+		leftear.render(f5);
+		rightear.render(f5);
+		tail.render(f5);
+		rightthumb.render(f5);
+		leftthumb.render(f5);
+		rightclaw1.render(f5);
+		rightclaw2.render(f5);
+		rightclaw3.render(f5);
+		leftclaw1.render(f5);
+		leftclaw2.render(f5);
+		leftclaw3.render(f5);
+		upperbody.render(f5);
+		lowerjaw.render(f5);
+		backjaw.render(f5);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
 }
