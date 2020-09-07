40077318fbc5c22a06541a5dde44bd9d4fafc52d
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-09-07 01:14:44 -0400

    Updated ScarabSwarm model
    ScarabSwarm model to 1.12.2

--- src/main/java/com/dev/wildycraft/client/model/ModelScarabSwarm.java
+++ src/main/java/com/dev/wildycraft/client/model/ModelScarabSwarm.java
@@ -1,12 +1,12 @@
 
-package wildycraft.client.model;
+package com.dev.wildycraft.client.model;
 
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.MathHelper;
+
 
 public class ModelScarabSwarm extends ModelBase
 {
